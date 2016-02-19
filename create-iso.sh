621e87c1a8f8369a42fc7c83f638f91a3a99e6ec
Joshua Barretto <barry.of.smith@gmail.com>
2016-02-19 20:46:54 +0000

    Added ISO creation script

---
+++ create-iso.sh
@@ -0,0 +1,7 @@
+DATE=`date --date="now" "+%Y-%m-%d"`
+
+mkdir -p isobuild/isodir/boot/grub
+cp src/thoth.bin isobuild/isodir/boot/thoth.bin
+cp grub.cfg isobuild/isodir/boot/grub/grub.cfg
+grub2-mkrescue -o isobuild/thoth-${DATE}.iso isobuild/isodir
+echo "Created Thoth ISO"
